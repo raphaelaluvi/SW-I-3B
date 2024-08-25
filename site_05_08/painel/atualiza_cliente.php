e9fbe5f1f754536a8b0d4dc2d50ee7798c490105
raphaelaluvi <raphaluvi@gmail.com>
2024-08-25 15:11:15 -0300

    deu erro no select

---
+++ site_05_08/painel/atualiza_cliente.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atualiza Clientes</title>
+    <link href="css/estilo.css" rel="stylesheet" />
+</head>
+<body>
+    
+    <h3>Atualize Clientes</h3>
+
+    <div>
+    <form action="/action_page.php">
+        <label for="fname">Nome</label>
+        <input type="text" id="nome" name="nome" placeholder="Digite o novo nome...">
+
+        <label for="lname">Email</label>
+        <input type="text" id="email" name="email" placeholder="seuemail@email.com">
+
+        <label for="lname">Telefone</label>
+        <input type="text" id="telefone" name="telefone" placeholder="(11) 98765-4321">
+    
+        <input type="submit" value="Enviar">
+    </form>
+    </div>
+
+</body>
+</html>
