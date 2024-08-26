d3be5b36dc2cf5143fb92c00d5cc752d78f2d6f9
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-08-26 08:52:37 -0300

    aula 26-08

--- site_05_08/painel/atualiza_cliente.php
+++ site_05_08/painel/atualiza_cliente.php
@@ -1,29 +1,57 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atualiza Clientes</title>
-    <link href="css/estilo.css" rel="stylesheet" />
-</head>
-<body>
-    
-    <h3>Atualize Clientes</h3>
+<?php
+    include 'conecta.php';
 
-    <div>
-    <form action="/action_page.php">
-        <label for="fname">Nome</label>
-        <input type="text" id="nome" name="nome" placeholder="Digite o novo nome...">
+    include 'menu.php';
+?>
+        <title>Atualizar Clientes</title>
 
-        <label for="lname">Email</label>
-        <input type="text" id="email" name="email" placeholder="seuemail@email.com">
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Atualizar Clientes</h1>
+                                               
+                        <div class="card mb-4">
+                        <form>
+                            <div class="mb-3">
+                                <label class="form-label">Nome</label>
+                                <input type="text" class="form-control">
+                            </div>
 
-        <label for="lname">Telefone</label>
-        <input type="text" id="telefone" name="telefone" placeholder="(11) 98765-4321">
-    
-        <input type="submit" value="Enviar">
-    </form>
-    </div>
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="email" class="form-control">
+                            </div>
 
-</body>
+                            <div class="mb-3">
+                                <label class="form-label">Telefone</label>
+                                <input type="email" class="form-control">
+                            </div>
+                            
+                            <button type="submit" class="btn btn-primary">Atualizar</button>
+                        </form>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
 </html>
